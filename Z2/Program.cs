27b35e4f97090494b295a816d8d4316c06cda7cc
Виктор <viktor.avgn@yandex.ru>
2023-02-28 23:15:24 +0700

    z2

--- Z2/Program.cs
+++ Z2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// a = любое число, b = любое число, необходимо указать, какое из них больше
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+ Console.WriteLine(a + " больше " + b);
+}
+else if (b > a)
+{
+ Console.WriteLine(b + " больше " + a);
+}
+else
+{
+ Console.WriteLine(b + " равно " + a);
+}
